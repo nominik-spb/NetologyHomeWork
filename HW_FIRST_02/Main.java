3264165c384b9c62542521278fceae63f78fe900
nominik-spb <61251151+nominik-spb@users.noreply.github.com>
2025-02-23 20:54:06 +0300

    Update Main.java

--- HW_FIRST_02/Main.java
+++ HW_FIRST_02/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-        public static void main(String[] args) {
-            int income = 325000;
-            int spending =282000;
-            System.out.println("Итого (руб): " + (income-spending));
-        }
+    public static void main(String[] args) {
+        int income = 325000;
+        int spending =282000;
+        System.out.println("Итого (руб): ");
+        System.out.println(income-spending);
+    }
 }
